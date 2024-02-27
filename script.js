200211fe1472d1c3f0689ef2b2db164919185762
Iskaban10 <136351084+Iskaban10@users.noreply.github.com>
2024-02-27 21:11:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+
+}
+
+
+
